682bf4436c267470b99cfcf83bc5b7774451f340
Rene Zeldenthuis <rzeldent@gmail.com>
2023-01-16 02:02:51 +0100

    Added reduce example

---
+++ examples/Reduce/Reduce.ino
@@ -0,0 +1,36 @@
+/***************************************************
+Copyright (c) 2019 Luis Llamas
+(www.luisllamas.es)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
+ ****************************************************/
+ 
+#include "ReactiveArduinoLib.h"
+using namespace Reactive;
+
+int values[] = { 0, 1, 4, 6, 2, 5, 7, 3, 5, 8 };
+int valuesLength = sizeof(values) / sizeof(values[0]);
+
+void setup()
+{
+	Serial.begin(115200);
+	while (!Serial) delay(1);
+}
+
+// Start with a float value of 10.0 (optional) and add 1.5 times the (integer) value
+
+void loop()
+{
+	FromArray(values, valuesLength)
+    .Reduce<float>([](float acc, int value) {
+        return acc + value * 1.5f;
+    }, 10.0f)
+	.DoAndFinally(
+		[](float x) { Serial.println(x); },
+		[]() { Serial.println("No more items"); }
+	);
+
+	delay(2000);
+}
